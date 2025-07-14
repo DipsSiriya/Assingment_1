72fc1949c0abff824d95582abb2bdbbc6ae3d809
DipsSiriya <dipeshsks2@gmail.com>
2025-07-14 12:34:47 +0530

    Add files via upload
    Task 1: Perform Basic Mathematical Operations
    Problem Statement: Write a Python program that does the following:
    1.  Takes two numbers as input from the user.
    2.  Performs the basic mathematical operations on these two numbers:
    o	Addition
    o	Subtraction
    o	Multiplication
    o	Division
    3.  Displays the results of each operation on the screen.
    
    Task 2: Create a Personalized Greeting
    Problem Statement: Write a Python program that:
    1.  Takes a user's first name and last name as input.
    2.  Concatenates the first name and last name into a full name.
    3.  Prints a personalized greeting message using the full name.

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+"""Task 1: Perform Basic Mathematical Operations
+"""
+
+a=int(input("Enter First No :"))
+b=int(input("Enter Second No :"))
+add=int(a+b)
+sub=int(a-b)
+mul=int(a*b)
+div=int(a/b)
+print("Addition :",add)
+print("Subtraction :",sub)
+print("Multiplication :",mul)
+print("Division :",div)
---
+++ Task_2.py
@@ -0,0 +1,14 @@
+"""Task 2: Create a Personalized Greeting
+"""
+fn=input("Enter your first name: ")
+ln=input("Enter your last name: ")
+msg=("Hello, "+fn+" "+ln+"! Welcome to the python program")
+print(msg)
+
+
+
+
+
+
+
+
